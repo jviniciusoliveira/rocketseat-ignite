c82362937ace7d1607d0e64439ecd66505ca5bad
José Vinicius <jose.vinicius@go.senac.br>
2021-03-15 19:46:43 -0300

    Segundo desafio técnico

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,26 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+
+import { TMovie } from './types';
+
+type ContentProps = {
+  title: string;
+  movies: TMovie[];
+}
+
+export function Content({ title, movies }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie: TMovie) => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
