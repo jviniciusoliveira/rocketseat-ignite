c913835e204fa14733499a119c8c7fd0d4e77f52
José Vinicius <jose.vinicius@go.senac.br>
2021-03-17 21:42:52 -0300

    transactions list

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,28 @@ createServer({
   models: {
     transaction: Model
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Web Site',
+          type: 'deposit',
+          category: 'Freela',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Home',
+          amount: 800,
+          createdAt: new Date('2021-02-01 09:00:00')
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = 'api'
 
