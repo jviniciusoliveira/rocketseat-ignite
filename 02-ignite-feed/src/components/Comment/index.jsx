3f0c439a96ddfcba32a44c85dbd3430ed33c8701
Jose Vinicius <jviniciusoliveira@gmail.com>
2023-01-24 17:41:20 -0300

    feat: add comment component

---
+++ 02-ignite-feed/src/components/Comment/index.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.commentContainer}>
+      <img src="https://github.com/jviniciusoliveira.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.commentAuthorAndTime}>
+              <strong>Jose Vinicius</strong>
+              <time title="20 de Janeiro às 8h" dateTime="2023-01-20 08:00:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- 02-ignite-feed/src/components/Post/index.jsx
+++ 02-ignite-feed/src/components/Post/index.jsx
@@ -1,3 +1,4 @@
+import { Comment } from '../Comment'
 import styles from './post.module.css'
 
 export function Post() {
@@ -44,6 +45,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.postCommentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
